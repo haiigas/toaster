9bb5385cab78c46af05f96f0d3333c412ffed1d3
Gagas Sangga Pratama <49256437+haiigas@users.noreply.github.com>
2025-05-03 16:01:09 +0700

    Update toaster.js

--- toaster.js
+++ toaster.js
@@ -1,28 +1,50 @@
 function extend(obj, ext) {
-    Object.keys(ext).forEach(function (key) { obj[key] = ext[key]; });
+    Object.keys(ext).forEach(function (key) {
+        obj[key] = ext[key];
+    });
     return obj;
 }
 
 function toaster(msg, ttype, opt) {
-    var ttype = (ttype) ? ttype : 'info', msi = '',
-        ticon = (ttype === 'info') ? 'fa fa-info-circle' : ((ttype === 'success') ? 'fa fa-check-circle' : ((ttype === 'error') ? 'fa fa-times-circle' : ((ttype === 'warning') ? 'fa fa-exclamation-circle' : ''))),
-        def = { position: 'bottom-right', ui: '', icon: 'auto', clear: false }, attr = (opt) ? extend(def, opt) : def;
+    const defaultType = 'info';
+    const icons = {
+        info: 'fa fa-info-circle',
+        success: 'fa fa-check-circle',
+        error: 'fa fa-times-circle',
+        warning: 'fa fa-exclamation-circle'
+    };
 
-    attr.position = (attr.position) ? 'toast-' + attr.position : 'toast-bottom-right';
-    attr.icon = (attr.icon === 'auto') ? ticon : ((attr.icon) ? attr.icon : '');
-    attr.ui = (attr.ui) ? ' ' + attr.ui : '';
+    ttype = ttype || defaultType;
+    const ticon = icons[ttype] || '';
 
-    msi = (attr.icon !== '') ? '<span class="toastr-icon"><em class="icon ' + attr.icon + '"></em></span>' : '',
-        msg = (msg !== '') ? msi + '<div class="toastr-text">' + msg + '</div>' : '';
+    const def = {
+        position: 'bottom-right',
+        ui: '',
+        icon: 'auto',
+        clear: false
+    };
+
+    const attr = opt ? extend(def, opt) : def;
+
+    // Set position and icon
+    attr.position = attr.position ? 'toast-' + attr.position : 'toast-bottom-right';
+    attr.icon = attr.icon === 'auto' ? ticon : (attr.icon || '');
+    attr.ui = attr.ui ? ' ' + attr.ui : '';
+
+    const msi = attr.icon ? `<span class="toastr-icon"><em class="icon ${attr.icon}"></em></span>` : '';
+    msg = msg ? msi + `<div class="toastr-text">${msg}</div>` : '';
 
     if (msg !== "") {
-        if (attr.clear === true) { toastr.clear(); }
-        var option = {
+        if (attr.clear === true) {
+            toastr.clear();
+        }
+
+        const options = {
             "closeButton": true,
             "debug": false,
             "newestOnTop": false,
             "progressBar": false,
-            "positionClass": attr.position + attr.ui,
+            "positionClass": `${attr.position}${attr.ui}`,
             "closeHtml": '<span class="btn-trigger">Close</span>',
             "preventDuplicates": true,
             "showDuration": "1500",
@@ -31,7 +53,8 @@ function toaster(msg, ttype, opt) {
             "toastClass": "toastr",
             "extendedTimeOut": "3000"
         };
-        toastr.options = extend(option, attr);
+
+        toastr.options = extend(options, attr);
         toastr[ttype](msg);
     }
 }
